7a388d4b11ea6b287463dd5c43cff8d2ab8a53d1
Charles DZADU <dkccharlie@gmail.com>
2022-11-16 18:30:44 +0100

    add ressources to project

---
+++ lib/src/frontend/themes/project_colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class ProjectColors {
+  static const Color primary = Color(0XFF161925);
+}
---
+++ lib/src/frontend/utils/project_images.dart
@@ -0,0 +1,7 @@
+class ProjectImages {
+  static const String onboarding1 = "assets/images/rafiki.png";
+  static const String onboarding2 = "assets/images/pana.png";
+  static const String onboarding3 = "assets/images/cuate.png";
+
+  static const String authIllustration = "assets/images/amico.png";
+}
